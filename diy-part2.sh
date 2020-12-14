a0bb8210193370b9de71dd5e87ed72430acb8ab8
wavelet001 <58981899+wavelet001@users.noreply.github.com>
2020-12-14 16:35:52 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,4 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.168.1/g' package/base-files/files/bin/config_generate
-set system.@system[-1].hostname='iKuai'
+sed -i 's/OpenWrt/iKuai/g' package/base-files/files/bin/config_generate
